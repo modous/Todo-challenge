0fbe1755b05e1edaf95b6559d0d006cbcc0a630e
Mohamed.T <mohamed1010@live.nl>
2024-02-13 10:48:21 +0100

    Feat: connected the API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
-import styles from "./page.module.css";
+// page.tsx
 import Layout from "./layout";
+import React from "react";
+import styles from "./page.module.css";
 
+async function getData() {
+  const res = await fetch(
+    "https://65c53ee5dae2304e92e41ae7.mockapi.io/api/todos/"
+  );
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
 
-export default function Home() {
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
   return (
     <Layout>
       <main className={styles.main}>
