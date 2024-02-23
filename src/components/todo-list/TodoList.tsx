264b4e26a5858214e56933a0146b8daee28fb966
Mohamed.T <mohamed1010@live.nl>
2024-02-23 10:59:29 +0100

    fix: changed index.module.css name

--- src/components/todo-list/TodoList.tsx
+++ src/components/todo-list/TodoList.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import styles from "./Index.module.css";
+import styles from "./index.module.css";
 import { TodoItem } from "../todo-item/TodoItem";
 
 interface ListProps {
