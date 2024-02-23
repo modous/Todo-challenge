5637b32da250725c9a2ab70ee671c4ee34650397
Mohamed.T <mohamed1010@live.nl>
2024-02-23 13:49:39 +0100

    fix: removed dependencies i don't use + removed checkboxClass
    I use css to define different styling voor de checkbox. I don't need the checkboxClas variable

--- src/components/Checkbox/checkbox.tsx
+++ src/components/Checkbox/checkbox.tsx
@@ -7,15 +7,13 @@ import styles from "./index.module.css";
 interface ICheckboxProps extends Omit<ComponentProps<"input">, "type"> {}
 
 export function Checkbox({ checked, onChange, ...props }: ICheckboxProps) {
-  const checkboxClass = checked ? styles.checked : styles.checkbox;
-
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     onChange?.(event);
   };
   return (
     <input
       {...props}
-      className={classnames({ checkboxClass }, styles.checkbox)}
+      className={classnames(styles.checkbox)}
       type="checkbox"
       checked={checked}
       onChange={handleChange}
