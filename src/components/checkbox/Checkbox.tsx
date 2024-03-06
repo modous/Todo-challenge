495209010e8c826e828ba27fc471bd64eabd9a0e
Mohamed.T <mohamed1010@live.nl>
2024-03-06 14:08:24 +0100

    Update file name

---
+++ src/components/checkbox/Checkbox.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { ChangeEvent, ComponentProps } from "react";
+import styles from "./index.module.css";
+import classnames from "classnames";
+
+interface ICheckboxProps extends ComponentProps<"input"> {
+  onCheckedChange?: (isChecked: boolean) => void;
+}
+
+export function Checkbox({
+  className,
+  onChange,
+  onCheckedChange,
+  ...props
+}: ICheckboxProps) {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange?.(event);
+    onCheckedChange?.(event.target.checked);
+  };
+
+  return (
+    <input
+      {...props}
+      className={classnames(styles.checkbox, className)}
+      type="checkbox"
+      onChange={handleChange}
+    />
+  );
+}
