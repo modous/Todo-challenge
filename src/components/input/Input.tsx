9e3de4df8778128b07c23f7d526beb3d6660cfc2
Mohamed.T <mohamed1010@live.nl>
2024-03-07 16:26:33 +0100

    fix: removed UNNECESSARY value in input component :)

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -5,6 +5,6 @@ interface IInputProps extends Omit<ComponentProps<"input">, "type"> {
   type?: "text" | "number" | "email" | "password" | "url";
 }
 
-export function Input({ value, type, ...props }: IInputProps) {
+export function Input({ type, ...props }: IInputProps) {
   return <input type={type} className={styles.input} {...props} />;
 }
