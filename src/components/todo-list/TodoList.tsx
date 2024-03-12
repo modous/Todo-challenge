23f984cfd0f544cc2f0df9b71964842a0c417b2f
Mohamed.T <mohamed1010@live.nl>
2024-03-12 10:30:07 +0100

    fix: start from scratch, now with library's

--- src/components/todo-list/TodoList.tsx
+++ src/components/todo-list/TodoList.tsx
@@ -3,60 +3,20 @@
 import { useState } from "react";
 import styles from "./index.module.css";
 import { TodoItem } from "../todo-item/TodoItem";
-import Draggable from "../draggable/Draggable";
-import Droppable from "../droppable/Droppable";
+import React from "react";
+import { useDrag } from "react-dnd";
 
 interface ListProps {
   data: ITodoItem[];
   onTodoChange: (id: number, state: ITodoItem) => void;
   onDeleteTodo: (id: number) => void;
-  onUpdateTodoOrder: (newOrder: number[]) => Promise<void>;
 }
 
 export default function TodoList({
   data,
   onTodoChange,
   onDeleteTodo,
-  onUpdateTodoOrder,
 }: ListProps) {
-  const [todoItems, setTodoItems] = useState<ITodoItem[]>(data);
-
-  const handleDrop = async (draggedItemId: number, dropIndex: number) => {
-    try {
-      const updatedTodoItems = [...todoItems];
-      const draggedItemIndex = updatedTodoItems.findIndex(
-        (item) => item.id === draggedItemId
-      );
-      console.log("Dragged Item Id:", draggedItemId);
-      console.log("Updated Todo Items:", updatedTodoItems);
-
-      // Log the id values of items in the array
-      console.log(
-        "Item IDs:",
-        updatedTodoItems.map((item) => item.id)
-      );
-      if (draggedItemIndex === -1) {
-        console.error("Dragged item not found in the current order.");
-        return;
-      }
-
-      const draggedItem = updatedTodoItems[draggedItemIndex];
-      updatedTodoItems.splice(draggedItemIndex, 1);
-
-      updatedTodoItems.splice(dropIndex, 0, draggedItem);
-
-      setTodoItems(updatedTodoItems);
-
-      // Prepare the new order to send to the server
-      const newOrder = updatedTodoItems.map((item) => item.id);
-
-      // Send a request to update the order of items on the server
-      await onUpdateTodoOrder(newOrder);
-    } catch (error) {
-      console.error("Failed to update todo order:", error);
-    }
-  };
-
   //This is the Empty state. If the array that i get from the Api is empty i return a paragraph
   if (data.length === 0) {
     return (
@@ -69,23 +29,19 @@ export default function TodoList({
   return (
     <ul className={styles.ulContainer}>
       {data.map((item, index) => (
-        <Droppable key={item.id} onDrop={() => handleDrop(index, item.id)}>
-          <li className={styles.listItemContainer} key={item.id}>
-            <Draggable itemID={item.id}>
-              <TodoItem
-                onCompletedChange={(completed: boolean) =>
-                  onTodoChange(item.id, { ...item, completed })
-                }
-                onDelete={() => onDeleteTodo(item.id)}
-                completed={item.completed}
-                title={item.title}
-                onTitleChange={(title: string) =>
-                  onTodoChange(item.id, { ...item, title })
-                }
-              />
-            </Draggable>
-          </li>
-        </Droppable>
+        <li className={styles.listItemContainer} key={item.id}>
+          <TodoItem
+            onCompletedChange={(completed: boolean) =>
+              onTodoChange(item.id, { ...item, completed })
+            }
+            onDelete={() => onDeleteTodo(item.id)}
+            completed={item.completed}
+            title={item.title}
+            onTitleChange={(title: string) =>
+              onTodoChange(item.id, { ...item, title })
+            }
+          />
+        </li>
       ))}
     </ul>
   );
