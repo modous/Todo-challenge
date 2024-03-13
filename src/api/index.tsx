18df0950d1d5d8a75b10a8b208092802fbde28a8
Mohamed.T <mohamed1010@live.nl>
2024-03-13 15:35:10 +0100

    feat: added api page

---
+++ src/api/index.tsx
@@ -0,0 +1,70 @@
+const API_URL = "https://65c53ee5dae2304e92e41ae7.mockapi.io/api/todos/";
+
+export async function getData() {
+  const result = await fetch(API_URL);
+
+  if (!result.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return result.json();
+}
+
+export async function addData(data: IAddTodoItemData) {
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to add data");
+    }
+
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+}
+
+export async function updateData(todoID: number, data: ITodoItem) {
+  try {
+    const response = await fetch(API_URL + `${todoID}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export async function deleteData(todoID: number) {
+  try {
+    const response = await fetch(API_URL + `${todoID}`, {
+      method: "Delete",
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to remove data");
+    }
+
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,76 +4,7 @@ import { useState, useEffect } from "react";
 import styles from "./index.module.css";
 import TodoList from "../components/todo-list/TodoList";
 import { AddTodoForm } from "@/components/add-todo-form";
-
-const API_URL = "https://65c53ee5dae2304e92e41ae7.mockapi.io/api/todos/";
-
-async function getData() {
-  const result = await fetch(API_URL);
-
-  if (!result.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return result.json();
-}
-
-async function addData(data: IAddTodoItemData) {
-  try {
-    const response = await fetch(API_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    if (!response.ok) {
-      throw new Error("Failed to add data");
-    }
-
-    const result = await response.json();
-
-    return result;
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
-  }
-}
-
-async function updateData(todoID: number, data: ITodoItem) {
-  try {
-    const response = await fetch(API_URL + `${todoID}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    const result = await response.json();
-
-    return result;
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
-async function deleteData(todoID: number) {
-  try {
-    const response = await fetch(API_URL + `${todoID}`, {
-      method: "Delete",
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to remove data");
-    }
-
-    const result = await response.json();
-
-    return result;
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
-  }
-}
+import { getData, addData, updateData, deleteData } from "../api";
 
 export default function Home() {
   const [todos, setTodos] = useState<ITodoItem[]>([]);
@@ -127,8 +58,12 @@ export default function Home() {
     <main className={styles.main}>
       <section className={styles.todoSection}>
         <h1 className={styles.title}>Todo list</h1>
-        <AddTodoForm onAddTodo={handleAddTodo}  />
-        <TodoList data={todos} onTodoChange={handleTodoChange} onDeleteTodo={handleDeleteTodo}/>
+        <AddTodoForm onAddTodo={handleAddTodo} />
+        <TodoList
+          data={todos}
+          onTodoChange={handleTodoChange}
+          onDeleteTodo={handleDeleteTodo}
+        />
       </section>
     </main>
   );
