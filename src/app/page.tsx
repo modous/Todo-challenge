5c77245695979f4f83012156f7baad4ff3221db9
Mohamed.T <mohamed1010@live.nl>
2024-03-13 16:01:11 +0100

    feat: correctly added the load state in the page file with useState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,17 @@ import { getData, addData, updateData, deleteData } from "../api";
 
 export default function Home() {
   const [todos, setTodos] = useState<ITodoItem[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     loadData();
   }, []);
 
   const loadData = async () => {
+    setLoading(true);
     const data = await getData();
     setTodos(data);
+    setLoading(false);
   };
 
   const handleTodoChange = async (id: number, state: ITodoItem) => {
@@ -59,11 +62,35 @@ export default function Home() {
       <section className={styles.todoSection}>
         <h1 className={styles.title}>Todo list</h1>
         <AddTodoForm onAddTodo={handleAddTodo} />
-        <TodoList
-          data={todos}
-          onTodoChange={handleTodoChange}
-          onDeleteTodo={handleDeleteTodo}
-        />
+        {loading ? (
+          <svg
+            className={styles.loadingState}
+            viewBox="0 0 100 100"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="rgba(153, 153, 153, 1)"
+              d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
+            >
+              <animateTransform
+                attributeName="transform"
+                attributeType="XML"
+                type="rotate"
+                dur="1s"
+                from="0 50 50"
+                to="360 50 50"
+                repeatCount="indefinite"
+              />
+            </path>
+          </svg>
+        ) : (
+          <TodoList
+            data={todos}
+            onTodoChange={handleTodoChange}
+            onDeleteTodo={handleDeleteTodo}
+          />
+        )}
       </section>
     </main>
   );
--- src/components/todo-list/TodoList.tsx
+++ src/components/todo-list/TodoList.tsx
@@ -15,46 +15,6 @@ export default function TodoList({
   onTodoChange,
   onDeleteTodo,
 }: ListProps) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(true); // Set loading to true when the component mounts or when data changes
-    // Simulate loading delay with setTimeout
-    const timeout = setTimeout(() => {
-      setLoading(false); // Set loading to false after a certain time
-    }, 150);
-
-    // Clean up function to clear timeout when component unmounts or when data changes
-    return () => clearTimeout(timeout);
-  }, [data]); // Re-run effect whenever data changes
-
-  if (loading) {
-    return (
-      <svg
-        className={styles.loadingState}
-        width="100px"
-        height="100px"
-        viewBox="0 0 100 100"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill="rgba(153, 153, 153, 1)"
-          d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
-        >
-          <animateTransform
-            attributeName="transform"
-            attributeType="XML"
-            type="rotate"
-            dur="1s"
-            from="0 50 50"
-            to="360 50 50"
-            repeatCount="indefinite"
-          />
-        </path>
-      </svg>
-    );
-  }
   //This is the Empty state. If the array that i get from the Api is empty i return a paragraph
   if (data.length === 0) {
     return <p className={styles.emptyState}>Create your first todo</p>;
