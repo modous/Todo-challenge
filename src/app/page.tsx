db42dbf4fbbf5ab92ef39a7f8a40b6f77d47b972
Mohamed.T <mohamed1010@live.nl>
2024-03-14 15:02:41 +0100

    feat: made loader component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@
 
 import { useState, useEffect } from "react";
 import styles from "./index.module.css";
-import TodoList from "../components/todo-list/TodoList";
+import { TodoList } from "../components/todo-list";
 import { AddTodoForm } from "@/components/add-todo-form";
 import { getData, addData, updateData, deleteData } from "../api";
+import { Loader } from "@/components/loader";
 
 export default function Home() {
   const [todos, setTodos] = useState<ITodoItem[]>([]);
@@ -63,27 +64,7 @@ export default function Home() {
         <h1 className={styles.title}>Todo list</h1>
         <AddTodoForm onAddTodo={handleAddTodo} />
         {loading ? (
-          <svg
-            className={styles.loadingState}
-            viewBox="0 0 100 100"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill="rgba(153, 153, 153, 1)"
-              d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
-            >
-              <animateTransform
-                attributeName="transform"
-                attributeType="XML"
-                type="rotate"
-                dur="1s"
-                from="0 50 50"
-                to="360 50 50"
-                repeatCount="indefinite"
-              />
-            </path>
-          </svg>
+          <Loader />
         ) : (
           <TodoList
             data={todos}
--- src/components/todo-list/TodoList.tsx
+++ src/components/todo-list/TodoList.tsx
@@ -10,11 +10,7 @@ interface ListProps {
   onDeleteTodo: (id: number) => void;
 }
 
-export default function TodoList({
-  data,
-  onTodoChange,
-  onDeleteTodo,
-}: ListProps) {
+export function TodoList({ data, onTodoChange, onDeleteTodo }: ListProps) {
   //This is the Empty state. If the array that i get from the Api is empty i return a paragraph
   if (data.length === 0) {
     return <p className={styles.emptyState}>Create your first todo</p>;
