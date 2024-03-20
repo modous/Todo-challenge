0306c1fb8134c57195460bff350c20968b299c47
Mohamed.T <mohamed1010@live.nl>
2024-03-20 14:55:45 +0100

    feat: added another test for the update todo + made an object of the api so that i can importy like api.getData + added error when id cannot be found in functionn

--- src/utils/updateTodos.spec.tsx
+++ src/utils/updateTodos.spec.tsx
@@ -2,18 +2,18 @@ const { updateTodos } = require("./update-todos");
 
 describe("test if the todos update correctly", () => {
   test("update todo with matching id", () => {
-    //make fake array with objects
+    //maak een fake array met objecten er in
     const prevTodos = [
       { id: 1, title: "Todo 1", completed: false },
       { id: 2, title: "Todo 2", completed: true },
       { id: 3, title: "Todo 3", completed: false },
     ];
 
-    //Make 2 new variables where the content of the items in the array change/update
+    //maak 2 nieuwe variabele waar je de content van de items in de array wijzigt
     const id = 3;
     const state = { title: "Updated Todo", completed: true };
 
-    //the test that the function renders and looks if the ID and completed match
+    //de test die de functie uitvoert en vergelijkt of de ID en completed klopt
     expect(updateTodos(prevTodos, id, state)).toEqual([
       { id: 1, title: "Todo 1", completed: false },
       { id: 2, title: "Todo 2", completed: true },
@@ -21,15 +21,18 @@ describe("test if the todos update correctly", () => {
     ]);
   });
   test("todo update when wrong ID", () => {
+    //maak een fake array met objecten er in
     const prevTodos = [
       { id: 1, title: "Todo 1", completed: false },
       { id: 2, title: "Todo 2", completed: true },
       { id: 3, title: "Todo 3", completed: false },
     ];
 
+    //maak 2 nieuwe variabele waar je de content van de items in de array wijzigt
     const id = -1;
     const state = { title: "Updated Todo", completed: true };
 
+    //de test die de functie uitvoert en vergelijkt of de ID en completed klopt
     expect(() => updateTodos(prevTodos, id, state)).toThrow(
       "Could not find todo with ID: -1"
     );
