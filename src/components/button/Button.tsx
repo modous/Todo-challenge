f3cf0b42a7a6e7370f88458e5cc8ec4b23017330
Mohamed.T <mohamed1010@live.nl>
2024-03-27 13:47:23 +0100

    Revert "fix: removed the components that showed up in github but not in vscode"
    This reverts commit 30fb72a0e46becde69ff8a3fa47de3e4354d6c6a.

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import classnames from "classnames";
+import styles from "./Index.module.css";
+import { ComponentProps } from "react";
+
+interface IButtonProps extends ComponentProps<"button"> {
+  size?: "sm" | "lg";
+  icon?: React.ReactNode;
+  variant?: "primary" | "secondary";
+}
+
+export function Button({
+  children,
+  size = "sm",
+  icon,
+  className,
+  variant = "primary",
+  ...props
+}: IButtonProps) {
+  return (
+    <button
+      className={classnames(
+        styles.button,
+        {
+          [styles.primary]: variant === "primary",
+          [styles.secondary]: variant === "secondary",
+          [styles.sm]: size === "sm",
+          [styles.lg]: size === "lg",
+          [styles.iconOnly]: icon && !children,
+        },
+        className
+      )}
+      {...props}
+    >
+      {icon && icon}
+      {children}
+    </button>
+  );
+}
