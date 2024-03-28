b90318a8e41f41647fc47f54c0d9197971eec4a3
Mohamed.T <mohamed1010@live.nl>
2024-03-28 10:09:39 +0100

    fix: added comment for test

--- src/utils/add-todo.tsx
+++ src/utils/add-todo.tsx
@@ -2,7 +2,7 @@ export function addTodo(todos: ITodoItem[], newTodo: ITodoItem) {
   if (newTodo.title.trim() === "") {
     throw Error("please provide a title");
   }
-
+  //dwadwad
   const newTodos = [...todos];
 
   newTodos.push(newTodo);
